3a6c65644668c2942aeaafdf1810c4f5495a31cd
ningenMe <amohkuru@gmail.com>
2022-09-10 09:37:02 +0900

    [fix]

--- codedeploy/script/deploy.sh
+++ codedeploy/script/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#aws ecr get-login-password --region AWS_REGION | docker login --username AWS --password-stdin AWS_ECR_REGISTRY
-#docker pull AWS_ECR_REGISTRY/nina-api-repository:latest
+aws ecr get-login-password --region AWS_REGION | docker login --username AWS --password-stdin AWS_ECR_REGISTRY
+docker pull AWS_ECR_REGISTRY/nina-api-repository:latest
