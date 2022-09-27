7f35c916195122fd3a2254191a0fcb51c4cec82d
ningenMe <amohkuru@gmail.com>
2022-09-28 06:52:48 +0900

    [add] envoy deploy

---
+++ envoy/codedeploy/script/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+aws ecr get-login-password --region AWS_REGION | docker login --username AWS --password-stdin AWS_ECR_REGISTRY
+
+docker pull AWS_ECR_REGISTRY/AWS_ECR_REPOSITORY:latest
+docker container stop nina-envoy-api | true
+docker container rm nina-envoy-api | true
+docker run -d -p8082:8082 --name nina-envoy-api AWS_ECR_REGISTRY/AWS_ECR_REPOSITORY:latest
+docker rmi `docker image ls | grep none | awk '{print $3}'` | true
