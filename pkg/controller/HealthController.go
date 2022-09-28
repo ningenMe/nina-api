d1813c51c99b02791b6a13438f7351c82ba7382d
ningenMe <amohkuru@gmail.com>
2022-09-29 05:40:57 +0900

    [fix]

--- pkg/controller/HealthController.go
+++ pkg/controller/HealthController.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"context"
+	"fmt"
 	nina_api_grpc "github.com/ningenMe/mami-interface/mami-generated-server/nina-api-grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
@@ -11,6 +12,7 @@ type HealthController struct {
 }
 
 func (c *HealthController) Get(ctx context.Context, empty *emptypb.Empty) (*nina_api_grpc.GetHealthResponse, error) {
+	fmt.Println("health")
 	return &nina_api_grpc.GetHealthResponse{
 		Message: "ok",
 	}, nil
