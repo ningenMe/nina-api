9b0d2d976f2e2df5289d6c206e3211b4e1a1b093
ningenMe <amohkuru@gmail.com>
2022-10-02 01:59:39 +0900

    [fix]

--- pkg/domainservice/ContributionService.go
+++ pkg/domainservice/ContributionService.go
@@ -14,7 +14,7 @@ var repository = infra.ContributionRepository{}
 func (s *ContributionService) GetStatistics(user string) *nina_api_grpc.GetStatisticsResponse {
 	mp := repository.GetSumMap(user)
 	year, month, day := time.Now().Date()
-	startAt := time.Date(2019, 1, 1, 0, 0, 0, 0, time.Local)
+	startAt := time.Date(2017, 12, 31, 0, 0, 0, 0, time.Local) //日曜日
 	endAt := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
 
 	createdPullRequestStatistics := GetStatusStatistics(startAt, endAt, user, "CREATED_PULL_REQUEST", mp)
@@ -32,21 +32,25 @@ func GetStatusStatistics(startAt time.Time, endAt time.Time, user string, status
 
 	var list []*nina_api_grpc.ContributionSum
 	totalSum := 0
-	for targetAt := startAt; targetAt.Before(endAt); targetAt = targetAt.AddDate(0,0,1) {
-		date := targetAt.Format("2006-01-02")
-		key := domainmodel.ContributionSumKey{
-			Date: date,
-			User: user,
-			Status: status,
-		}
+	for targetWeekAt := startAt; targetWeekAt.Before(endAt); targetWeekAt = targetWeekAt.AddDate(0,0,7) {
 
 		sum := 0
-		if _, ok := mp[key]; ok {
-			sum = mp[key]
+		for i := 0; i < 7; i++ {
+			targetDayDate := targetWeekAt.AddDate(0,0,i).Format("2006-01-02")
+			key := domainmodel.ContributionSumKey{
+				Date: targetDayDate,
+				User: user,
+				Status: status,
+			}
+			if _, ok := mp[key]; ok {
+				sum += mp[key]
+			}
+
 		}
 
+		targetWeekDate := targetWeekAt.Format("2006-01-02")
 		list = append(list, &nina_api_grpc.ContributionSum{
-			Date: date,
+			Date: targetWeekDate,
 			Sum: int32(sum),
 		})
 		totalSum += sum
